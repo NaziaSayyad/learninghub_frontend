cd60f48b5ad489f78b46be261ba6ae160bf82e78
Nazia Sayyad <sayyadnazia257@gmai.com>
2023-06-24 22:06:18 +0530

    slideshow-updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,35 +5,28 @@ import Slideshow from './components/Slideshow/Slideshow';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [isMobile, setIsMobile] = useState(false);
+  // const [isMobile, setIsMobile] = useState(false);
 
-  // Update isMobile state based on window width
-  const handleResize = () => {
-    setIsMobile(window.innerWidth <= 768);
-  };
+  // // Update isMobile state based on window width
+  // // Update is Mobile  jk handleResize = ()=>{
 
-  // Listen for window resize events
-  useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    handleResize();
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+  // // }
+  // const handleResize = () => {
+  //   setIsMobile(window.innerWidth <= 768);
+  // };
+
+  // // Listen for window resize events
+  // useEffect(() => {
+  //   window.addEventListener('resize', handleResize);
+  //   handleResize();
+  //   return () => window.removeEventListener('resize', handleResize);
+  // }, []);
+
   return (
     <>
       <Navbar />
-        {
-          isMobile? (
-            <Grid templateRows={'repeat, 1fr'}>
-           <Slideshow />
-          <Slideshow />
-        </Grid>
-          ) : 
-          <Flex>
-           <Slideshow />
-          <Slideshow />
-        </Flex>
-        }
-    {/* <Headers /> */}
+        
+    <Headers />
     </>
   );
 }
--- my-app/src/Pages/Headers/headers.jsx
+++ my-app/src/Pages/Headers/headers.jsx
@@ -1,12 +1,45 @@
-import { Box, Heading } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Flex, Grid, Heading } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { useEffect } from 'react';
+import Slideshow from '../../components/Slideshow/Slideshow';
 
 function Headers() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Update isMobile state based on window width
+  // Update is Mobile  jk handleResize = ()=>{
+
+  // }
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+
+  // Listen for window resize events
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
   return (
    <>
    <Box>
-     <Heading> Welcome to Iqra Learning Hub: Your Gateway to Knowledge and Growth! </Heading>
+     <Heading
+      textAlign={'center'}
+      >
+       Welcome to Iqra Learning Hub: Your Gateway to Knowledge and Growth! </Heading>
    </Box>
+   {
+          isMobile? (
+            <Grid templateRows={'repeat, 1fr'}>
+           <Slideshow />
+          <Slideshow />
+        </Grid>
+          ) : 
+          <Flex>
+           <Slideshow />
+          <Slideshow />
+        </Flex>
+        }
    </>
   )
 }
