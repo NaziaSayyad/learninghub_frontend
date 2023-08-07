ed18d2e05eec434f93422d5ca74e5dc2f2d94027
Nazia Sayyad <sayyadnazia257@gmai.com>
2023-08-07 19:03:53 +0530

    some-changes

--- my-app/src/Pages/About/About.jsx
+++ my-app/src/Pages/About/About.jsx
@@ -1,10 +1,12 @@
-import { Heading } from '@chakra-ui/react'
+import { Heading, Text } from '@chakra-ui/react'
 import React from 'react'
 
 function About() {
   return (
     <>
     <Heading> About Page </Heading>
+    <Text>  This is an About page, In this page, we have told about us. </Text>
+    <Text>  We have provided all the necessary details about us all the necessary links </Text>
     </>
   )
 }
