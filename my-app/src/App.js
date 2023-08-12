5e24ae4143fd120effef3e746f7a952e2abc8b1b
Nazia Sayyad <sayyadnazia257@gmai.com>
2023-08-12 21:00:09 +0530

    changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 
 import Navbar from './components/Navbar/Navbar';
 import AllRoutes from './AllRoutes/AllRoutes';
+import { Heading } from '@chakra-ui/react';
 
 function App() {
   return (
     <>
         <Navbar />
         <AllRoutes />
+        <Heading > Main page </Heading>
     </>
   );
 }
