f39b0aab82135e0c638dfd673534670e0eb6140f
Nazia Sayyad <sayyadnazia257@gmai.com>
2023-08-17 21:23:25 +0530

    update

--- my-app/src/Pages/Home/Home.jsx
+++ my-app/src/Pages/Home/Home.jsx
@@ -22,61 +22,6 @@ function Home() {
     <Body
           images={images} interval={interval}
     />
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
-    <Heading>  My  Page  </Heading>
-    <Heading>  My  slideshow page </Heading>
     <Heading>  My  Page  </Heading>
   
     </>
