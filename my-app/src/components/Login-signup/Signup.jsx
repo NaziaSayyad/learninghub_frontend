70866e9615b21fed610e5bf8b1e69a630416a3f9
Nazia Sayyad <sayyadnazia257@gmai.com>
2023-08-29 20:49:34 +0530

    update

--- my-app/src/components/Login-signup/Signup.jsx
+++ my-app/src/components/Login-signup/Signup.jsx
@@ -1,10 +1,31 @@
-import { Heading } from '@chakra-ui/react'
+import { Heading, Text } from '@chakra-ui/react'
 import React from 'react'
 
 function Signup() {
   return (
+   <>
    <Heading> Signup page </Heading>
+   <Heading>  This is a Signup page </Heading>
+   <Text>  This is for Signup  j kj kj  j kj hj   kh kjh kj  hgj h gj </Text>
+   <Text>        hui huu    kj this kfskaj kj sa jkdsa jksahd kjads khsdalkdsaj kdsalk f sakjh dksh kjads 
+     </Text>
+     <Text> Technical skills </Text>
+    <Text>  Physics wallah </Text> 
+    <Text> Turmeric syphon</Text>
+    <Text> Can you provide a single bhjhjk fakj afjk afkj kj kjak sakj kjas jakjhkj 
+      akjaskj kjahjkja kjfafsak kjhfkaskk
+    </Text>
+    <Text> khkjhc kj  jkh   dfskj khds kjj kj khj k kj </Text>
+    <Text>  khsfkh khfs kj vkhzk kz kjzx kj kjzvc kjz kjz </Text>
+    <Text> fzlk lzfdj lkjzd lkjfdz kjlzfd lkjfzd lkjfzdlkj lkjfz lkjfz lkjfdz lkjfdzl</Text>
+    <Heading>  Projects </Heading>
+    <Text> this is a project section </Text>
+    
+   </>
+
   )
 }
+/*    
+*/
 
 export default Signup
